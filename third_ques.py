b7764e7e6d35834a38f904c23a07d1ed8cd889b8
Vibhor Bhatnagar <vibhourbhatnagar7@gmail.com>
2021-11-13 13:17:16 +0530

    third question edit

--- third_ques.py
+++ third_ques.py
@@ -8,5 +8,18 @@ Explanation- as third dictionary has maximum value 11 so it comes first, then se
 with value 8 and last dictionary with value 6.'''
 
 l2=[{1:4,2:6,9:5},{2:3,4:6,3:8},{6:0,9:11,3:1}]
-l2.sort(reverse = False)
-print(l2) 
+max_list = list()
+for i in l2:    # loop to find max value from each dictionary
+    max_list.append(max(list(i.values())))
+max_list.sort(reverse=True) # sort max values of each dict to descending order
+solution = []
+inserted_value = list()
+for i in max_list:
+    for j in l2:
+        if i in list(j.values()):   
+            if not inserted_value:  # if value unique
+                solution.append(j)
+            elif i not in inserted_value:
+                solution.append(j)
+            inserted_value.append(i)
+print(solution)
